27a0a2700f7be9993a4bb3ce81441eb9594a2715
Daniel Canup <daniel@finidev.tech>
2021-10-08 20:16:04 -0600

    Update function.js

--- randomizer/function.js
+++ randomizer/function.js
@@ -1,6 +1,53 @@
-window.function = function (str) {
+// window.function = function (str) {
+//   str = str.value ?? "";
+//   return str.random();
+// }
 
-  str = str.value ?? "";
+var theNames = [ "Mark", "Corinne", "Brittany", "Carson", "John", "Mike", "Jordan", "Whitney", "Josh", "Sabian", "Ty", "Leslie" ];
+var sortedList = theNames.sort();
 
-  return str.random();
-}
+$(document).ready(function() {
+	
+	$('.numerator, .denominator').text($(theNames).length);
+	for (i = 0; i < sortedList.length; i++) {
+		
+		$("<li>" + sortedList[i] + "</li>").appendTo($('#the-names'));
+	};
+	
+	$("#run-it").on('click', function() {
+		
+		function chooseRando() {
+			
+			var randomPick = Math.floor(Math.random() * theNames.length) + 1; 
+			var pickedLI = $('#the-names li:nth-child(' + randomPick + ')').text();
+			if (!$('#the-names li:nth-child('+randomPick+')').hasClass('crossed')) {
+				
+				$('#the-names li:nth-child('+randomPick+')').addClass('crossed');
+				$('#selected-name').text(pickedLI);
+				$('.numerator').text($('#the-names li:not(.crossed)').length);
+				
+			} else {
+				
+				chooseRando();
+			}
+		}
+		
+		if (!$('#the-names li:not(.crossed)').length) {
+
+			alert('You are out of names!!');
+			$('#run-it').addClass('all-done');
+
+		} else {
+
+			chooseRando();
+		}
+	})
+	
+	$('#start-over').on('click', function() {
+	
+		$("#the-names li.crossed").removeClass('crossed');
+		$("#selected-name").text("");
+		$('.numerator, .denominator').text($(theNames).length);
+		
+	})
+}) // doc ready
