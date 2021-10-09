31268b1b5878cb5a26067ad0ca1b67cbfb1db1c4
Daniel Canup <daniel@finidev.tech>
2021-10-08 22:27:45 -0600

    Update function.js

--- randomizer/function.js
+++ randomizer/function.js
@@ -1,53 +1,112 @@
-// window.function = function (str) {
-//   str = str.value ?? "";
-//   return str.random();
-// }
+"use strict";
 
-var theNames = [ "Mark", "Corinne", "Brittany", "Carson", "John", "Mike", "Jordan", "Whitney", "Josh", "Sabian", "Ty", "Leslie" ];
-var sortedList = theNames.sort();
+// Change to false if you don't want a timer
+const showTimer = true;
 
-$(document).ready(function() {
-	
-	$('.numerator, .denominator').text($(theNames).length);
-	for (i = 0; i < sortedList.length; i++) {
-		
-		$("<li>" + sortedList[i] + "</li>").appendTo($('#the-names'));
-	};
-	
-	$("#run-it").on('click', function() {
-		
-		function chooseRando() {
-			
-			var randomPick = Math.floor(Math.random() * theNames.length) + 1; 
-			var pickedLI = $('#the-names li:nth-child(' + randomPick + ')').text();
-			if (!$('#the-names li:nth-child('+randomPick+')').hasClass('crossed')) {
-				
-				$('#the-names li:nth-child('+randomPick+')').addClass('crossed');
-				$('#selected-name').text(pickedLI);
-				$('.numerator').text($('#the-names li:not(.crossed)').length);
-				
-			} else {
-				
-				chooseRando();
-			}
-		}
-		
-		if (!$('#the-names li:not(.crossed)').length) {
+// Set timer countdown time here in minutes : seconds format
+const time = 0 + ":" + 19;
 
-			alert('You are out of names!!');
-			$('#run-it').addClass('all-done');
+// Add list of names here
+const namesList = [
+'Anne',
+'Bob',
+'Catherine',
+'Dave',
+'Erin',
+'Frank',
+'Gloria'];
 
-		} else {
 
-			chooseRando();
-		}
-	})
-	
-	$('#start-over').on('click', function() {
-	
-		$("#the-names li.crossed").removeClass('crossed');
-		$("#selected-name").text("");
-		$('.numerator, .denominator').text($(theNames).length);
-		
-	})
-}) // doc ready
+// Default variables
+let i = 0;
+let x = 0;
+let intervalHandle = null;
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const headerOne = document.getElementById('headerNames');
+const timesUp = document.getElementById('timesUp');
+const timerWrapper = document.getElementById('timerWrapper');
+const timer = document.getElementById('timer');
+
+// Optional countdown timer
+// Add zero in front of numbers < 10
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec;
+  }
+  if (sec < 0) {
+    sec = "59";
+  }
+  return sec;
+}
+
+const startTimer = function () {
+  const presentTime = timer.innerHTML;
+  const timeArray = presentTime.split(/[:]+/);
+  let m = timeArray[0];
+  let s = checkSecond(timeArray[1] - 1);
+
+  if (s == 59) {
+    m = m - 1;
+  }
+  if (m < 0) {
+    timesUp.style.display = "block";
+  }
+
+  timer.innerHTML = m + ":" + s;
+
+  setTimeout(startTimer, 1000);
+};
+
+// Start or stop the name shuffle on button click
+startButton.addEventListener('click', function () {
+  this.style.display = "none";
+  stopButton.style.display = "block";
+  intervalHandle = setInterval(function () {
+    headerNames.textContent = namesList[i++ % namesList.length];
+  }, 50);
+  if (showTimer === true) {
+    timerWrapper.classList.remove('visible');
+  }
+});
+stopButton.addEventListener('click', function () {
+  this.style.display = "none";
+  startButton.style.display = "block";
+  clearInterval(intervalHandle);
+  timer.innerHTML = time;
+  if (showTimer === true) {
+    timerWrapper.classList.add('visible');
+  }
+  startTimer();
+});
+
+// Allow use of spacebar to start/stop name shuffle
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    if (x % 2 === 0) {
+      startButton.style.display = "none";
+      stopButton.style.display = "block";
+      intervalHandle = setInterval(function () {
+        headerNames.textContent = namesList[i++ % namesList.length];
+      }, 50);
+      if (showTimer === true) {
+        timerWrapper.classList.remove('visible');
+      }
+    } else {
+      startButton.style.display = "block";
+      stopButton.style.display = "none";
+      clearInterval(intervalHandle);
+      timer.innerHTML = time;
+      if (showTimer === true) {
+        timerWrapper.classList.add('visible');
+      }
+      startTimer();
+    }
+    x++;
+  }
+};
+
+// Blinking warning
+var backgroundInterval = setInterval(function () {
+  timesUp.classList.toggle("backgroundRed");
+}, 1000);
